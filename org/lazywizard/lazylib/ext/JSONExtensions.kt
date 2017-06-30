495a051daf1f7f4475bae7fbc081a47a90ce7740
Robert Maguire <LazyWizard@users.noreply.github.com>
2017-06-30 06:40:35 -0500

    Inlined extension methods (need to test w/ decompiler to see if this helps)

--- org/lazywizard/lazylib/ext/JSONExtensions.kt
+++
@@ -1,7 +0,0 @@
-package org.lazywizard.lazylib.ext
-
-import org.json.JSONObject
-
-fun JSONObject.getFloat(key: String): Float = this.getDouble(key).toFloat()
-fun JSONObject.optFloat(key: String): Float = this.optDouble(key).toFloat()
-fun JSONObject.optFloat(key: String, defaultValue: Float): Float = this.optDouble(key, defaultValue.toDouble()).toFloat()
